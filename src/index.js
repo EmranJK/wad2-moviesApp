1f5cb6ac594fe524ebebbab0555da141d02719d2
Emran Sabbagh <emran2156@gmail.com>
2021-10-31 00:57:31 +0100

    Add new review support.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import AddMovieReviewPage from './pages/addMovieReviewPage'
 import SiteHeader from './components/siteHeader'
 import MoviesContextProvider from "./contexts/moviesContext";
 import MovieReviewPage from "./pages/movieReviewPage";
@@ -27,6 +28,7 @@ const App = () => {
         <MoviesContextProvider>
             {" "}
             <Switch>
+        <Route exact path="/reviews/form" component={AddMovieReviewPage} />
       <Route path="/reviews/:id" component={MovieReviewPage} />
         <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
         <Route path="/movies/:id" component={MoviePage} />
